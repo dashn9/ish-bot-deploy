ae7b445456d26be972a65a493476e9ff88f4d8fb
Daniel Shogbon <ishogbon@gmail.com>
2024-11-15 01:11:12 +0100

    updated to arm

--- scripts/k8s/install_control_plane.sh
+++ scripts/k8s/install_control_plane.sh
@@ -5,17 +5,17 @@ echo && echo "$0: " && echo
 
 # Install/prepare etcd
 wget -q --show-progress --https-only --timestamping \
-    "https://storage.googleapis.com/etcd/v3.5.15/etcd-v3.5.15-linux-amd64.tar.gz"
-tar -xvf etcd-v3.5.15-linux-amd64.tar.gz
-sudo mv etcd-v3.5.15-linux-amd64/etcd* /usr/local/bin/
+    "https://storage.googleapis.com/etcd/v3.5.15/etcd-v3.5.15-linux-arm64.tar.gz"
+tar -xvf etcd-v3.5.15-linux-arm64.tar.gz
+sudo mv etcd-v3.5.15-linux-arm64/etcd* /usr/local/bin/
 
 # Provision Kubernetes Control Plane
 sudo mkdir -p /etc/kubernetes/config
 wget -q --show-progress --https-only --timestamping \
-  "http://dl.k8s.io/v1.31.0/bin/linux/amd64/kube-apiserver" \
-  "https://dl.k8s.io/v1.31.0/bin/linux/amd64/kube-controller-manager" \
-  "https://dl.k8s.io/v1.31.0/bin/linux/amd64/kube-scheduler" \
-  "https://dl.k8s.io/v1.31.0/bin/linux/amd64/kubectl"
+  "http://dl.k8s.io/v1.31.0/bin/linux/arm64/kube-apiserver" \
+  "https://dl.k8s.io/v1.31.0/bin/linux/arm64/kube-controller-manager" \
+  "https://dl.k8s.io/v1.31.0/bin/linux/arm64/kube-scheduler" \
+  "https://dl.k8s.io/v1.31.0/bin/linux/arm64/kubectl"
 chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl
 sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
 } >> ./control_plane_installation.log
--- scripts/k8s/install_worker.sh
+++ scripts/k8s/install_worker.sh
@@ -9,13 +9,13 @@ sudo apt-get install -y gnupg socat conntrack ipset ca-certificates
 
 # Install/Configure Worker Dependencies
 wget -q --show-progress --https-only --timestamping \
-    https://dl.k8s.io/${KUBE_LATEST}/bin/linux/amd64/kube-proxy \
-    https://dl.k8s.io/${KUBE_LATEST}/bin/linux/amd64/kubelet \
-    https://dl.k8s.io/release/${KUBE_LATEST}/bin/linux/amd64/kubectl \
-    https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.31.1/crictl-v1.31.1-linux-amd64.tar.gz \
-    https://github.com/opencontainers/runc/releases/download/v1.1.14/runc.amd64 \
-    https://github.com/containernetworking/plugins/releases/download/v1.5.1/cni-plugins-linux-amd64-v1.5.1.tgz \
-    https://github.com/containerd/containerd/releases/download/v1.7.21/containerd-1.7.21-linux-amd64.tar.gz 
+    https://dl.k8s.io/${KUBE_LATEST}/bin/linux/arm64/kube-proxy \
+    https://dl.k8s.io/${KUBE_LATEST}/bin/linux/arm64/kubelet \
+    https://dl.k8s.io/release/${KUBE_LATEST}/bin/linux/arm64/kubectl \
+    https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.31.1/crictl-v1.31.1-linux-arm64.tar.gz \
+    https://github.com/opencontainers/runc/releases/download/v1.1.14/runc.arm64 \
+    https://github.com/containernetworking/plugins/releases/download/v1.5.1/cni-plugins-linux-arm64-v1.5.1.tgz \
+    https://github.com/containerd/containerd/releases/download/v1.7.21/containerd-1.7.21-linux-arm64.tar.gz 
 
 sudo mkdir -p \
     containerd \
@@ -28,10 +28,10 @@ sudo mkdir -p \
 
 
 
-tar -xvf crictl-v1.31.1-linux-amd64.tar.gz
-sudo tar -xvf containerd-1.7.21-linux-amd64.tar.gz -C containerd
-sudo tar -xvf cni-plugins-linux-amd64-v1.5.1.tgz  -C /opt/cni/bin/
-mv runc.amd64 runc
+tar -xvf crictl-v1.31.1-linux-arm64.tar.gz
+sudo tar -xvf containerd-1.7.21-linux-arm64.tar.gz -C containerd
+sudo tar -xvf cni-plugins-linux-arm64-v1.5.1.tgz  -C /opt/cni/bin/
+mv runc.arm64 runc
 sudo chmod +x crictl kubectl kube-proxy kubelet runc 
 sudo mv crictl kubectl kube-proxy kubelet runc /usr/local/bin/
 sudo mv containerd/bin/* /bin/
